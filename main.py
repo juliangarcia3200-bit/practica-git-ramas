a2ee12584a059b7241c5a2d1d31d83e4ac75e525
DarwinSanta82 <gerencia@inteligenciadedatos.com>
2025-08-11 18:46:27 -0500

    Listo Mi Comentario Darwin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 mensajes = ["Ejemplo: Ana - Primera practica en Github"
             "Julian - Hola mundo",
+            "Darwin Github es genial por que puedo guardar mis proyectos y compartirlos con mis amigos, ademas de que puedo ver los proyectos de otras personas"
             ]
 
 for m in mensajes:
